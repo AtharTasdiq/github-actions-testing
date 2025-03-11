c80bd12942e3a7b0a4a2fc011d7c3de6bf86b0fe
AtharTasdiq <atharkhantasdiq@gmail.com>
2025-03-11 17:19:07 +0600

    update test.py to add a greeting message and comment out API request code

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ API_KEY = os.environ.get('API_KEY')
 # set up the request parameters
 #new comment hello
 
-url = f'https://data.unwrangle.com/api/getter/?platform=homedepot_detail&url=https%3A%2F%2Fwww.homedepot.com%2Fp%2FSunnydaze-Decor-10-5-ft-Fabric-Cotton-Double-Brazilian-Hammock-with-Stand-Combo-in-Beach-Oasis-DB-Combo-Oasis%2F306248639&api_key={API_KEY}'
-response = requests.get(url)
-print(response.json())
+# url = f'https://data.unwrangle.com/api/getter/?platform=homedepot_detail&url=https%3A%2F%2Fwww.homedepot.com%2Fp%2FSunnydaze-Decor-10-5-ft-Fabric-Cotton-Double-Brazilian-Hammock-with-Stand-Combo-in-Beach-Oasis-DB-Combo-Oasis%2F306248639&api_key={API_KEY}'
+# response = requests.get(url)
+# print(response.json())
+print("hello world")
