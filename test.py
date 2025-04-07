e6a6b6eb14f55d5678bc9a7ae4893bb464bd0c47
AtharTasdiq <atharkhantasdiq@gmail.com>
2025-04-07 17:00:49 +0600

    update test.py to add a new comment for clarity

--- test.py
+++ test.py
@@ -9,4 +9,5 @@ API_KEY = os.environ.get('API_KEY')
 # url = f'https://data.unwrangle.com/api/getter/?platform=homedepot_detail&url=https%3A%2F%2Fwww.homedepot.com%2Fp%2FSunnydaze-Decor-10-5-ft-Fabric-Cotton-Double-Brazilian-Hammock-with-Stand-Combo-in-Beach-Oasis-DB-Combo-Oasis%2F306248639&api_key={API_KEY}'
 # response = requests.get(url)
 # print(response.json())
+# some changes
 print("hello world")
